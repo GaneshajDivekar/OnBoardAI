158e49436ef3c8544e408b1326f5c32edad8fa51
ganeshdivekar <ganeshajdivekar@gmail.com>
2025-02-09 00:37:41 +0530

    Added env

--- routes/chatbot.py
+++ routes/chatbot.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-from services.chatbot_service import get_mistral_response
+from services.chatbot_service import get_response
 from schemas import ChatRequest
 
 router = APIRouter()
 
+
 @router.post("/chat/")
 async def chat(request: ChatRequest):
-    response = await get_mistral_response(request.message)
+    response = await get_response(request.message)
     return {"response": response}
+
