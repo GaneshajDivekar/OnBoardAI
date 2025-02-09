24b0aa81c480fbea03671797bb8a892d827b37fe
ganeshdivekar <ganeshajdivekar@gmail.com>
2025-02-09 15:23:33 +0530

    onboarding manage

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from fastapi import FastAPI
-from database import test_connection
-import asyncio
-from routes import documents, training, chatbot
 from fastapi.middleware.cors import CORSMiddleware
+
+from database import test_connection
+from routes import documents, training, chatbot, onboarding
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],
+    allow_headers=["*"], )
+
 
 # ✅ Use lifespan event instead of asyncio.run()
 @app.on_event("startup")
 async def startup():
     await test_connection()  # ✅ This will run on FastAPI's event loop
 
+
 app.include_router(documents.router, prefix="/api")
 app.include_router(training.router, prefix="/api")
 app.include_router(chatbot.router, prefix="/api")
+app.include_router(onboarding.router, prefix="/api")
+
 
 @app.get("/")
 def read_root():
